52d0608b7aec8c8e212b9cf2f8e7869d979d53eb
Rodrigo Pires <pires.rodrigo@outlook.com.br>
2025-06-16 15:48:59 -0300

    Pilha.java

---
+++ src/Pilha.java
@@ -0,0 +1,54 @@
+public class Pilha {
+
+    private No inicio, atual, aux;
+    private int size;
+
+    public Pilha() {
+        this.inicio = null;
+        this.atual = null;
+        this.size = 0;
+    }
+
+    public void push(double valor) {
+        aux = new No(valor);
+        if (size == 0) {
+            inicio = aux;
+            atual = inicio;
+        } else {
+            atual.setProx(aux);
+            aux.setAnt(atual);
+            atual = aux;
+        }
+        size++;
+    }
+
+    public double pop() {
+        if (empty()) {
+            throw new IllegalStateException("Pilha vazia");
+        }
+        aux = atual;
+        double valor = aux.getValor();
+
+        if (atual == inicio) {
+            inicio = null;
+            atual = null;
+        } else {
+            atual = atual.getAnt();
+            atual.setProx(null);
+        }
+
+        aux.setAnt(null);
+        aux.setProx(null);
+
+        size--;
+        return valor;
+    }
+
+    public boolean empty() {
+        return size == 0;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
